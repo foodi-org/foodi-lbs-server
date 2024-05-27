a895af5774d4028c2e6ef1487d5eaecca8480ba8
odinfor <747069227@qq.com>
2024-05-27 13:42:22 +0800

    commit: register devliver server

--- foodilbs.go
+++ foodilbs.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/foodi-org/foodi-lbs-server/github.com/foodi-org/foodi-lbs-server"
 	"github.com/foodi-org/foodi-lbs-server/internal/config"
+	deliverServer "github.com/foodi-org/foodi-lbs-server/internal/server/delivery"
 	geoServer "github.com/foodi-org/foodi-lbs-server/internal/server/geo"
 	"github.com/foodi-org/foodi-lbs-server/internal/svc"
 
@@ -30,8 +31,11 @@ func main() {
 	// 注册服务
 	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
 
-		foodi_lbs_server.RegisterGeoServer(grpcServer, geoServer.NewGeoServer(svc.Svc())) // lbs位置服务
-		foodi_lbs_server.RegisterDeliveryServer(grpcServer)
+		// lbs位置服务
+		foodi_lbs_server.RegisterGeoServer(grpcServer, geoServer.NewGeoServer(svc.Svc()))
+
+		// 配送位置服务，作为服务分层注册示例
+		foodi_lbs_server.RegisterDeliveryServer(grpcServer, deliverServer.NewDeliveryServer(svc.Svc()))
 
 		if c.Mode == service.DevMode || c.Mode == service.TestMode {
 			reflection.Register(grpcServer)
